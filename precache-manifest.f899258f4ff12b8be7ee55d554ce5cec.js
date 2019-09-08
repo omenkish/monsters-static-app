c0fbade265a56fd6aef0e9418425e92bc2b44f57
omenkish <omenkish@gmail.com>
2019-09-08 17:12:08 +0100

    Updates

---
+++ precache-manifest.f899258f4ff12b8be7ee55d554ce5cec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8008f267d0302561184b1bd3089b11c",
+    "url": "/monsters-static-app/index.html"
+  },
+  {
+    "revision": "227023e9d9439c3f2e96",
+    "url": "/monsters-static-app/static/css/main.b17f58cd.chunk.css"
+  },
+  {
+    "revision": "cea528bd8a6c6ccbefab",
+    "url": "/monsters-static-app/static/js/2.9918f956.chunk.js"
+  },
+  {
+    "revision": "227023e9d9439c3f2e96",
+    "url": "/monsters-static-app/static/js/main.0329cc6b.chunk.js"
+  },
+  {
+    "revision": "3fdcfc9ebd36bedc4299",
+    "url": "/monsters-static-app/static/js/runtime~main.af195fba.js"
+  }
+]);
